2f30ead9f1e207747b683467eade2849d0872316
Damian404 <sgt,curren@gmail.com>
2018-04-08 06:01:08 +0100

    get postions of chars in alphabet

--- src/ie/gmit/sw/ai/Decryptor.java
+++ src/ie/gmit/sw/ai/Decryptor.java
@@ -6,12 +6,16 @@ public class Decryptor {
 		int i;
 		int len = r.length;
 		char c1, c2;
+		int c1_ind, c2_ind;
 
 		for (i = 0; i < len; i += 2) {
 			c1 = r[i];
 			c2 = r[i + 1];
+			
+			c1_ind = (int)(new String(child).indexOf(c1));
+			c2_ind = (int)(new String(child).indexOf(c2));
 
-			System.out.println(c1 + " " + c2);
+			System.out.println(c1_ind + " " + c2_ind);
 		}
 		return k;
 	}
