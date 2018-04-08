e5b2ad8e7629b4327a69c7e0f4d1d60e4c5bc300
Damian404 <sgt,curren@gmail.com>
2018-04-08 05:23:28 +0100

    read file quadgrams contaning a collection of quadgrams and their occurence

--- src/ie/gmit/sw/ai/CipherBreaker.java
+++ src/ie/gmit/sw/ai/CipherBreaker.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 public class CipherBreaker {
 	public static void main(String[] args) throws IOException {
 		String content = FileParser.gramReader("cyphertext.txt").readLine();
+		BufferedReader input2 = FileParser.gramReader("4grams.txt");
 		
 		System.out.println(content);
+		System.out.println(input2.readLine());
 	}
 }
