aa8724d5ac1faf876292aa357391272c30860940
Damian404 <sgt,curren@gmail.com>
2018-04-08 10:34:08 +0100

    comments

--- src/ie/gmit/sw/ai/HeuristicValue.java
+++ src/ie/gmit/sw/ai/HeuristicValue.java
@@ -3,19 +3,26 @@ package ie.gmit.sw.ai;
 import java.util.Map;
 
 public class HeuristicValue {
+	//method to get heuristic log score of current key
 	public static double totalScore(Map<String, Integer> quadgrams, long totalQuadgrams, char[] ks)
 	{
 		double totalScore = 0;
 		int frequency = 0;
 		
+		//changes passed in char[] array, which is the current decrypted text attempt
 		String k = new String(ks);
 		
+		//loop through text, -4 because we are counting in +4's
 		for(int i=0; i< k.length() - 4; i++){
+			//check if 4grams.txt contains current read quadgram from decrypted text
 			if(quadgrams.get(k.substring(i, i+4)) != null){
+				//frequency of quadgram in 4grams.txt
 				frequency = quadgrams.get(k.substring(i, i+4));
 			}else{
+				//if current uadgram is not found in 4gram.txt
 				frequency = 1;
 			}
+			//get score of occurence/totalgrams and floor the score using log10
 			totalScore += Math.log10((double) frequency/totalQuadgrams);
 		}
 		
--- src/ie/gmit/sw/ai/QuadGrams.java
+++ src/ie/gmit/sw/ai/QuadGrams.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class QuadGrams {
+	//method to assign 4grams.txt contents to Map<String, Integer>
 	public static Map<String, Integer> readQuadgrams(BufferedReader input2) throws IOException
 	{
 		Map<String, Integer> quadgrams = new HashMap<String, Integer>();
@@ -13,20 +14,26 @@ public class QuadGrams {
 		int total = 0;
 		String add = "";
 		
+		//loop until end of file
 		for(int i = 0; i < 389373; i++)
 		{
 			String values = input2.readLine();
+			//add # to create an end point to the line
 			values += "#";
+			//get first 4 characters of each line
 			String gram = "" + values.charAt(0) + values.charAt(1) + values.charAt(2) + values.charAt(3);
 			int j = 5;
 			total = 0;
 			add = "";
+			//adds character which is an int postion to a string
 			while(values.charAt(j) != '#')
 			{
 				add += values.charAt(j);
 				j++;
 			}
+			//convert the string of an integer number to an int, total
 			total = Integer.parseInt(add);
+			//put quadgram and occurence of quadgram into map
 			quadgrams.put(gram, total);
 		}
 		
