632103e6d5c9086bf45d95231a1039c955463e48
Damian404 <sgt,curren@gmail.com>
2018-04-08 06:57:53 +0100

    added user menu

--- src/ie/gmit/sw/ai/CipherBreaker.java
+++ src/ie/gmit/sw/ai/CipherBreaker.java
@@ -2,17 +2,48 @@ package ie.gmit.sw.ai;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CipherBreaker {
 	public static void main(String[] args) throws IOException {
-		String content = FileParser.gramReader("cyphertext.txt").readLine();
-		BufferedReader input2 = FileParser.gramReader("4grams.txt");
+		boolean exit = false;
+		Scanner scanner = new Scanner(System.in);
+		String fname = "";
 		
-		String validContent = FileParser.validateText(content);
-		
-		char[] r = validContent.toCharArray();
-		char[] k = validContent.toCharArray();
-		
-		SimulatedAnnealing.saStart(r, k, validContent, input2);
+		while(exit == false)
+		{
+			System.out.println("===============Menu==============");
+		    System.out.println("Select one of the following options below");
+		    System.out.println("1: Enter text file name to decrypt");
+		    System.out.println("2: Exit");
+			
+		    int choice = scanner.nextInt();
+		    
+		    switch (choice) {
+		        case 1:
+		            System.out.print("Name: ");
+		            fname = scanner.next();
+		            fname += ".txt";
+		            
+		            String content = FileParser.gramReader(fname).readLine();
+		            
+		            BufferedReader input2 = FileParser.gramReader("4grams.txt");
+		    		
+		    		String validContent = FileParser.validateText(content);
+		    		
+		    		char[] r = validContent.toCharArray();
+		    		char[] k = validContent.toCharArray();
+		    		
+		    		SimulatedAnnealing.saStart(r, k, validContent, input2);
+		            
+		            break;
+		        case 2:
+		            exit = true;
+		            break;
+		        default:
+		    }
+		}
+
+	    scanner.close();
 	}
 }
