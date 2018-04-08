dae78c84d77719e59b89a2284ae7f964b5bdaa8f
Damian404 <sgt,curren@gmail.com>
2018-04-08 06:13:16 +0100

    setting best score variable

--- src/ie/gmit/sw/ai/SimulatedAnnealing.java
+++ src/ie/gmit/sw/ai/SimulatedAnnealing.java
@@ -23,13 +23,13 @@ public class SimulatedAnnealing {
 	public static void simulatedAnnealing(char[] r, char[] k, String content, Map<String, Integer> quadgrams)
 	{
 		char[] child = parent.clone();
-		double maxScore;
+		double maxScore, bestScore;
 		long totalQuadgrams = quadgrams.values().stream().mapToLong(i->i).sum();
 		
 		k = Decryptor.decipher(r, k, child);
 		
 		maxScore = HeuristicValue.totalScore(quadgrams, totalQuadgrams, k);
 		
-		System.out.println(maxScore);
+		bestScore = maxScore;
 	}
 }
